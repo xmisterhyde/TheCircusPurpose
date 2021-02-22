ce2a9bc90569f489f26c34a71b3846017af74ceb
Mister Hyde <79381027+xmisterhyde@users.noreply.github.com>
2021-02-22 03:46:34 -0500

    Add files via upload

---
+++ TheCircusPurpose.py
@@ -0,0 +1,103 @@
+#!/user/bin/python
+
+import os
+os.system("cls")
+print ("""                                                                              \u001b[31m/////(((((((((////*,                                                                                  
+                                                                          ,*//(/,.((((((#((((((((//**.                                                                              
+                                                                        .,..(((((#############((((///*,                                                                             
+                                                                        *//(((,##,#,.,(,#(#,,,#,(,*.//*                                                                             
+                                                                        *.,/(,,#################((((//*,                                                                            
+                                                                       ,*/(*.......,.,###.,........    ,                                                                            
+                                                                        / .(,.###(.,,,####*.#####* /((/*                                                                            
+                                                                        ((###.#####%%%&%%%######((.((((/                                                                            
+                                                                        ((....   *../#%%##(/...        /.                                                                           
+                                                                        (            /###(/.           ,*                                                                           
+                                                                       *(.          ./(##//..          /*                                                                           
+                                                                      //((#......./#(/(##/*(##*...,..((,                                                                            
+                                                                       *. ###.,#####((,,,,//(#####(. (/                                                                             
+                                                                       ,*((##.*####/((,,,,,//###(((( //.                                                                            
+                                                                        ,,/(((/########(((####(((((/, .           .                                                                 
+                                                                          ,/(((((###########(((((/. *..                                                                             
+                                                                          ,,*/,*(((((######(((((/.***,.                                                                             
+                                                                           ,*//((.    ....*,.  ///**,                                                                               
+                                                                             *//((//.  ....  /////*,                                                                                
+                                                                             (.*/((((######(((//*,                                                                                  
+                                                                                 ,/(((((#((((/*                                                                                     
+                                                                                     *////*                                                                                
+\u001b[35m$$$$$$$$\ $$\                  $$$$$$\  $$\                                         $$$$$$$\                                                              
+\__$$  __|$$ |                $$  __$$\ \__|                                        $$  __$$\                                                             
+   $$ |   $$$$$$$\   $$$$$$\  $$ /  \__|$$\  $$$$$$\   $$$$$$$\ $$\   $$\  $$$$$$$\ $$ |  $$ |$$\   $$\  $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$$\  $$$$$$\  
+   $$ |   $$  __$$\ $$  __$$\ $$ |      $$ |$$  __$$\ $$  _____|$$ |  $$ |$$  _____|$$$$$$$  |$$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ 
+   $$ |   $$ |  $$ |$$$$$$$$ |$$ |      $$ |$$ |  \__|$$ /      $$ |  $$ |\$$$$$$\  $$  ____/ $$ |  $$ |$$ |  \__|$$ /  $$ |$$ /  $$ |\$$$$$$\  $$$$$$$$ |
+   $$ |   $$ |  $$ |$$   ____|$$ |  $$\ $$ |$$ |      $$ |      $$ |  $$ | \____$$\ $$ |      $$ |  $$ |$$ |      $$ |  $$ |$$ |  $$ | \____$$\ $$   ____|
+   $$ |   $$ |  $$ |\$$$$$$$\ \$$$$$$  |$$ |$$ |      \$$$$$$$\ \$$$$$$  |$$$$$$$  |$$ |      \$$$$$$  |$$ |      $$$$$$$  |\$$$$$$  |$$$$$$$  |\$$$$$$$\ 
+   \__|   \__|  \__| \_______| \______/ \__|\__|       \_______| \______/ \_______/ \__|       \______/ \__|      $$  ____/  \______/ \_______/  \_______|
+                                                                                                                  $$ |                                    
+                                                                                                                  $$ |                                    
+                                                                                                                  \__|  \u001b[0m                                  
+                                          \u001b[33m a doxx formatting tool written by mister hyde <@xmisterhyde on instagram>
+                                           note: for any information you do not have, simply input none, or unknown.\u001b[0m""")
+print()
+print()
+print()
+huntername = input('\u001b[31m[TCP@user]: \u001b[34menter your name. (note: this will be visible. if you are anonymous, use your pseudonym.)\n\u001b[35m[user@TCP]: \u001b[33m')
+prefix = input('\u001b[31m[TCP@user]: \u001b[34menter the prefix you would like to appear on your doxx. this could be your name, symbol, etc.\n\u001b[35m[user@TCP]: \u001b[33m')
+targetfirst = input('\u001b[31m[TCP@user]: \u001b[34menter the first name of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetlast = input('\u001b[31m[TCP@user]: \u001b[34menter the last name of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetage = input('\u001b[31m[TCP@user]: \u001b[34menter the age of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetsex = input('\u001b[31m[TCP@user]: \u001b[34menter the sex of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetphone = input('\u001b[31m[TCP@user]: \u001b[34menter the phone number of your target (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetip = input('\u001b[31m[TCP@user]: \u001b[34menter the ip of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetinstagram = input('\u001b[31m[TCP@user]: \u001b[34menter the instagram account of your target. (the person you are doxxing, without the @ symbol.)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetfacebook = input('\u001b[31m[TCP@user]: \u001b[34menter the facebook account of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetcountry = input('\u001b[31m[TCP@user]: \u001b[34menter the country of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetstate = input('\u001b[31m[TCP@user]: \u001b[34menter the state/province of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetcity = input('\u001b[31m[TCP@user]: \u001b[34menter the city of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetzip = input('\u001b[31m[TCP@user]: \u001b[34menter the zip code of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetaddress = input('\u001b[31m[TCP@user]: \u001b[34menter the street address of your target. (the person you are doxxing)\n\u001b[35m[user@TCP]: \u001b[33m')
+targetreason = input('\u001b[31m[TCP@user]: \u001b[34menter the reason you are doxxing your target.\n\u001b[35m[user@TCP]: \u001b[33m') 
+print ()
+print ()
+print ()
+print ()
+print ('\u001b[31m[TCP@user]: \u001b[34minformation received.')
+print ('\u001b[31m[TCP@user]: \u001b[34mgenerating doxx..............')
+print ()
+print ()
+print ()                     
+print ("""\u001b[35m$$$$$$$$\ $$\                  $$$$$$\  $$\                                         $$$$$$$\                                                              
+\__$$  __|$$ |                $$  __$$\ \__|                                        $$  __$$\                                                             
+   $$ |   $$$$$$$\   $$$$$$\  $$ /  \__|$$\  $$$$$$\   $$$$$$$\ $$\   $$\  $$$$$$$\ $$ |  $$ |$$\   $$\  $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$$\  $$$$$$\  
+   $$ |   $$  __$$\ $$  __$$\ $$ |      $$ |$$  __$$\ $$  _____|$$ |  $$ |$$  _____|$$$$$$$  |$$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|$$  __$$\ 
+   $$ |   $$ |  $$ |$$$$$$$$ |$$ |      $$ |$$ |  \__|$$ /      $$ |  $$ |\$$$$$$\  $$  ____/ $$ |  $$ |$$ |  \__|$$ /  $$ |$$ /  $$ |\$$$$$$\  $$$$$$$$ |
+   $$ |   $$ |  $$ |$$   ____|$$ |  $$\ $$ |$$ |      $$ |      $$ |  $$ | \____$$\ $$ |      $$ |  $$ |$$ |      $$ |  $$ |$$ |  $$ | \____$$\ $$   ____|
+   $$ |   $$ |  $$ |\$$$$$$$\ \$$$$$$  |$$ |$$ |      \$$$$$$$\ \$$$$$$  |$$$$$$$  |$$ |      \$$$$$$  |$$ |      $$$$$$$  |\$$$$$$  |$$$$$$$  |\$$$$$$$\ 
+   \__|   \__|  \__| \_______| \______/ \__|\__|       \_______| \______/ \_______/ \__|       \______/ \__|      $$  ____/  \______/ \_______/  \_______|
+                                                                                                                  $$ | <@xmisterhyde on instagram>                               
+                                                                                                                  $$ |                                    
+                                                                                                                  \__| \u001b[0m""")
+print ('\u001b[33m[',prefix,']\u001b[34m---\u001b[31mFIRST NAME:\u001b[35m', targetfirst)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m----\u001b[31mLAST NAME:\u001b[35m', targetlast)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m-----\u001b[31mAGE:\u001b[35m', targetage)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m------\u001b[31mSEX:\u001b[35m', targetsex)
+print()
+print ('\u001b[33m[',prefix,']\u001b[34m-------\u001b[31mPHONE NUMBER:\u001b[35m', targetphone)
+print ('\u001b[33m[',prefix,']\u001b[34m------\u001b[31mINSTAGRAM:\u001b[35m @',targetinstagram)
+print ('\u001b[33m[',prefix,']\u001b[34m-----\u001b[31mFACEBOOK:\u001b[35m', targetfacebook)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m----\u001b[31mCOUNTRY:\u001b[35m', targetcountry)
+print ('\u001b[33m[',prefix,']\u001b[34m---\u001b[31mSTATE/PROVICE:\u001b[35m', targetstate)
+print ('\u001b[33m[',prefix,']\u001b[34m----\u001b[31mCITY:\u001b[35m', targetcity) 
+print ('\u001b[33m[',prefix,']\u001b[34m-----\u001b[31mZIP CODE:\u001b[35m', targetzip)
+print ('\u001b[33m[',prefix,']\u001b[34m------\u001b[31mADDRESS:\u001b[35m', targetaddress)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m-------\u001b[31mREASON FOR DOXX:\u001b[35m', targetreason)
+print ()
+print ('\u001b[33m[',prefix,']\u001b[34m--------\u001b[31mDOXXED BY:\u001b[35m', huntername)
+print ()
+print ('\u001b[31m[TCP@user]:\u001b[33m PROGRAM: TheCircusPurpose by Mister Hyde (https://github.com/xmisterhyde/TheCircusPurpose)') 
+print ('\u001b[31m[TCP@user]:\u001b[33m AUTHOR: Mister Hyde. <@xmisterhyde on Instagram.> "Whats the matter boys, you dont like circus clowns?"')   
+print()
